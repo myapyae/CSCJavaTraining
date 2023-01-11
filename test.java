e489e59a369bc90ab7c8c02f8d4746f2ffec6e60
myapyae <myapyaemon@gmail.com>
2023-01-11 09:55:01 +0630

    test1

---
+++ test.java
@@ -0,0 +1,22 @@
+package Operator_Test;
+
+public class test {
+
+	public static void main(String[] args) {
+		int i=12;//Declaration of i
+		int iInc;//Declaration of iInc
+		
+		
+	    
+	    iInc=i++;//increment after operation
+	    System.out.println("the value of iInc " + iInc);
+	    System.out.println("the value of i " + i);
+	    
+	    iInc=++i;//increment before operation
+	    System.out.println("the value of iInc " + iInc);
+	    System.out.println("the value of i " + i);
+	  
+
+	}
+
+}
